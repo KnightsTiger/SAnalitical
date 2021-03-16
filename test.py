25652061bd561f30d0fcdf9be7d8f111faaa1634
Knight <tocaldera6734@gmail.com>
2021-03-16 08:31:02 +0530

    data extraction url added

--- test.py
+++ test.py
@@ -2,3 +2,5 @@ from googleapiclient.discovery import build
 api_key = 'AIzaSyCTH8yqSkChtMmnEmKdBsVm4n3BxSNrsLg'
 youtube = build('youtube','V3',developerKey=api_key)
 print(type(youtube))
+# following is the data extraction URL
+#https://www.googleapis.com/youtube/v3/channels?part=statistics&id=UCFbNIlppjAuEX4znoulh0Cw&key=AIzaSyCTH8yqSkChtMmnEmKdBsVm4n3BxSNrsLg
